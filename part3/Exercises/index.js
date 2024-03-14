6401b05d53eab4f58a5bf81f61d2d6bdd503ff1f
TonyTong999 <105336587+TonyTong999@users.noreply.github.com>
2024-03-13 22:50:13 -0700

    phonebook 3.3 -3.6 add retrieving/delete/post specific person && duplication name check

--- part3/Exercises/index.js
+++ part3/Exercises/index.js
@@ -7,7 +7,7 @@ const PORT = 3001
     console.log(`Server running on port ${PORT}`)
   })
 
-let phonebook =[
+let persons =[
   { 
     "id": 1,
     "name": "Arto Hellas", 
@@ -37,13 +37,68 @@ app.get('/info', (request, response) => {
   const time = now.toLocaleTimeString('en-US');
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   const htmlResponse = `
-  <p>Phone book has info for ${phonebook.length} people</p>
+  <p>Phone book has info for ${persons.length} people</p>
   <p> ${day} ${date} ${time} Time zone: ${timeZone}</p>
 `;
   response.send(htmlResponse)
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(phonebook)
+  response.json(persons)
 })
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if(person){
+  console.log(person.name)
+  response.json(person)
+  }else{
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+  if(persons.find(person => person.name === body.name) !== undefined){
+    return response.status(400).json({
+       error: 'name must be unique' 
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name, 
+    number: body.number,
+    
+  }
+
+  persons = persons.concat(person)
+  console.log(person)
+  response.json(person)
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 10000)
+}
+
 
